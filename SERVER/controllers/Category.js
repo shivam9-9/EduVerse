ac620b9b7a64383ddffd6ccc95b6a26ff8c4e3dd
shivam9-9 <shivamdubey5163@gmail.com>
2025-03-09 12:14:36 +0530

    added more controllers and functionalities

--- SERVER/controllers/Category.js
+++ SERVER/controllers/Category.js
@@ -1,11 +1,46 @@
 const Category = require("../models/Category");
 
 exports.createCategory = async (req, res) => {
-
+	try {
+		const { name, description } = req.body;
+		if (!name) {
+			return res
+				.status(400)
+				.json({ success: false, message: "All fields are required" });
+		}
+		const CategorysDetails = await Category.create({
+			name: name,
+			description: description,
+		});
+		console.log(CategorysDetails);
+		return res.status(200).json({
+			success: true,
+			message: "Categorys Created Successfully",
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: true,
+			message: error.message,
+		});
+	}
 };
+
 exports.showAllCategories = async (req, res) => {
-
+	try {
+        console.log("INSIDE SHOW ALL CATEGORIES");
+		const allCategorys = await Category.find({});
+		res.status(200).json({
+			success: true,
+			data: allCategorys,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
 };
+
 
 //categoryPage detail
 exports.categoryPageDetails = async (req, res) => {
