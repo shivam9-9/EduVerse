b00a059022fbc2db03898f0b01b1d7ce1b17a845
shivam9-9 <shivamdubey5163@gmail.com>
2025-05-24 17:20:23 +0530

    fixed bugs

--- SERVER/index.js
+++ SERVER/index.js
@@ -9,51 +9,58 @@ const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {cloudinaryConnect } = require("./config/cloudinary");
+const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-//database connect
+// Database connection
 database.connect();
-//middlewares
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
+		origin: "http://localhost:3000",
+		credentials: true,
 	})
-)
+);
 
 app.use(
 	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
+		useTempFiles: true,
+		tempFileDir: "/tmp",
 	})
-)
-//cloudinary connection
+);
+
+// Cloudinary connection
 cloudinaryConnect();
 
-//routes
+// Routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
-//def route
+// Serve static files for production build
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../build/index.html"));
+});
 
+// Default route
 app.get("/", (req, res) => {
 	return res.json({
-		success:true,
-		message:'Your server is up and running....'
+		success: true,
+		message: "Your server is up and running....",
 	});
 });
 
 app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
-
+	console.log(`App is running at ${PORT}`);
+});
